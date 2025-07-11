bf0e6c249e74d06cfaf124df8c3fe360932cbd46
Paulo <paulofilhorocha22@gmail.com>
2025-07-11 19:50:55 -0300

    filterArray

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ console.log(array)
 let novo = [...array];
 novo.pop()
 console.log(novo)
+
+let  filterArray = [1,2,3,40,50,100,20,40,50];
+
+const novoFilter = filterArray.filter ((num) => num >= 10 )
+console.log(filterArray)
+console.log(novoFilter)
