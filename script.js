7ee13d9bf0d5d431bb62cd440d6d0ffd287622c4
Paulo <paulofilhorocha22@gmail.com>
2025-07-11 19:55:58 -0300

    filterArray

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ let  filterArray = [1,2,3,40,50,100,20,40,50];
 const novoFilter = filterArray.filter ((num) => num >= 10 )
 console.log(filterArray)
 console.log(novoFilter)
+
+
+
+//outro exemplos,palavras com mais de 4 letras
+
+const palavras = ['sol', 'lua', 'planeta', 'estrela', 'céu'];
+
+// Crie um array com palavras que tenham mais de 4 letras.
+
+const novasPalavras =  palavras.filter ((palavra) => palavra.length > 4 )
+console.log(novasPalavras)
